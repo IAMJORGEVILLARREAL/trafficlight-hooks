4f09a8cf24987fa3bfe66dc12d1a096d60faccdc
IAMJORGEVILLARREAL <jvillarrealalfredo@gmail.com>
2023-03-21 00:02:24 +0000

    commit

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+
+const TrafficLight = () => {
+  const [selectedLight, setSelectedLight] = useState(null);
+
+  const handleLightClick = (color) => {
+    setSelectedLight(color);
+  };
+
+  return (
+    <div className="traffic-light">
+      <div
+        className={`light red ${selectedLight === "red" ? "active" : ""}`}
+        onClick={() => handleLightClick("red")}
+      ></div>
+      <div
+        className={`light yellow ${
+          selectedLight === "yellow" ? "active" : ""
+        }`}
+        onClick={() => handleLightClick("yellow")}
+      ></div>
+      <div
+        className={`light green ${selectedLight === "green" ? "active" : ""}`}
+        onClick={() => handleLightClick("green")}
+      ></div>
+    </div>
+  );
+};
+
+export default TrafficLight;
